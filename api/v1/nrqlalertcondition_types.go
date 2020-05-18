bc697d71010b144e7a31ece7ec8fb7534620b645
Zach Leslie <zleslie@newrelic.com>
2020-05-18 12:58:45 -0700

    chore(api): update types for NerdGrph

--- api/v1/nrqlalertcondition_types.go
+++ api/v1/nrqlalertcondition_types.go
@@ -1,34 +1,37 @@
 package v1
 
 import (
-	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/newrelic/newrelic-client-go/pkg/alerts"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NrqlAlertConditionSpec defines the desired state of NrqlAlertCondition
 type NrqlAlertConditionSpec struct {
-	Terms               []AlertConditionTerm `json:"terms,omitempty"`
-	Nrql                NrqlQuery            `json:"nrql,omitempty"`
-	Type                string               `json:"type,omitempty"`
-	Name                string               `json:"name,omitempty"`
-	RunbookURL          string               `json:"runbook_url,omitempty"`
-	ValueFunction       string               `json:"value_function,omitempty"`
-	PolicyID            int                  `json:"-"`
-	ID                  int                  `json:"id,omitempty"`
-	ViolationCloseTimer int                  `json:"violation_time_limit_seconds,omitempty"`
-	ExpectedGroups      int                  `json:"expected_groups,omitempty"`
-	IgnoreOverlap       bool                 `json:"ignore_overlap,omitempty"`
-	Enabled             bool                 `json:"enabled"`
-	ExistingPolicyID    int                  `json:"existing_policy_id,omitempty"`
-	APIKey              string               `json:"api_key,omitempty"`
-	APIKeySecret        NewRelicAPIKeySecret `json:"api_key_secret,omitempty"`
-	Region              string               `json:"region,omitempty"`
+	APIKey             string                                 `json:"api_key,omitempty"`
+	APIKeySecret       NewRelicAPIKeySecret                   `json:"api_key_secret,omitempty"`
+	AccountID          int                                    `json:"account_id,omitempty"`
+	Description        string                                 `json:"description,omitempty"`
+	Enabled            bool                                   `json:"enabled"`
+	ExistingPolicyID   int                                    `json:"existing_policy_id,omitempty"`
+	ExpectedGroups     int                                    `json:"expected_groups,omitempty"`
+	ID                 int                                    `json:"id,omitempty"`
+	IgnoreOverlap      bool                                   `json:"ignore_overlap,omitempty"`
+	Name               string                                 `json:"name,omitempty"`
+	Nrql               alerts.NrqlConditionQuery              `json:"nrql,omitempty"`
+	PolicyID           int                                    `json:"-"`
+	Region             string                                 `json:"region,omitempty"`
+	RunbookURL         string                                 `json:"runbookUrl,omitempty"`
+	Terms              []AlertConditionTerm                   `json:"terms,omitempty"`
+	Type               alerts.NrqlConditionType               `json:"type,omitempty"`
+	ValueFunction      *alerts.NrqlConditionValueFunction     `json:"valueFunction,omitempty"`
+	ViolationTimeLimit alerts.NrqlConditionViolationTimeLimit `json:"violationTimeLimit,omitempty"`
 }
 
 // NrqlQuery represents a NRQL query to use with a NRQL alert condition
+// TODO replace with alerts.NrqlConditionQuery
 type NrqlQuery struct {
 	Query      string `json:"query,omitempty"`
 	SinceValue string `json:"since_value,omitempty"`
@@ -36,11 +39,12 @@ type NrqlQuery struct {
 
 // AlertConditionTerm represents the terms of a New Relic alert condition.
 type AlertConditionTerm struct {
-	Duration     resource.Quantity `json:"duration,omitempty"`
-	Operator     string            `json:"operator,omitempty"`
-	Priority     string            `json:"priority,omitempty"`
-	Threshold    resource.Quantity `json:"threshold"`
-	TimeFunction string            `json:"time_function,omitempty"`
+	Operator             alerts.NrqlConditionOperator `json:"operator,omitempty"`
+	Priority             alerts.NrqlConditionPriority `json:"priority,omitempty"`
+	Threshold            string                       `json:"threshold,omitempty"`
+	ThresholdDuration    int                          `json:"threshold_duration,omitempty"`
+	ThresholdOccurrences alerts.ThresholdOccurrence   `json:"threshold_occurrences,omitempty"`
+	TimeFunction         string                       `json:"time_function,omitempty"`
 }
 
 // NrqlAlertConditionStatus defines the observed state of NrqlAlertCondition
@@ -80,12 +84,88 @@ func init() {
 	SchemeBuilder.Register(&NrqlAlertCondition{}, &NrqlAlertConditionList{})
 }
 
-func (in NrqlAlertConditionSpec) APICondition() alerts.NrqlCondition {
-	jsonString, _ := json.Marshal(in)
-	var APICondition alerts.NrqlCondition
-	json.Unmarshal(jsonString, &APICondition) //nolint
+func (in NrqlAlertConditionSpec) APIConditionBase() alerts.NrqlConditionBase {
+	conditionBase := alerts.NrqlConditionBase{
+		Description:        in.Description,
+		Enabled:            in.Enabled,
+		Name:               in.Name,
+		Nrql:               in.Nrql,
+		RunbookURL:         in.RunbookURL,
+		ViolationTimeLimit: in.ViolationTimeLimit,
+	}
 
-	//APICondition.PolicyID = spec.ExistingPolicyId
+	for _, term := range in.Terms {
+		t := alerts.NrqlConditionTerms{}
 
-	return APICondition
+		t.Operator = term.Operator
+		t.Priority = term.Priority
+		// TODO handle the float here
+
+		// When parsing a string
+		f, err := strconv.ParseFloat(term.Threshold, 64)
+		if err != nil {
+			log.Error(err, "strconv.ParseFloat()", "threshold", term.Threshold)
+		}
+
+		// When parsing a resource.Quantity
+		// j, err := json.Marshal(term.Threshold)
+		// if err != nil {
+		// 	log.Error(err, "json marshal", "threshold", term.Threshold)
+		// }
+
+		// var f float64
+		// err = json.Unmarshal(j, &f) //nolint
+		// if err != nil {
+		// 	log.Error(err, "json unmarshal", "j", string(j))
+		// }
+
+		t.Threshold = f
+		t.ThresholdDuration = term.ThresholdDuration
+		t.ThresholdOccurrences = term.ThresholdOccurrences
+
+		fmt.Printf("t: %+v\n\n\n", t)
+
+		conditionBase.Terms = append(conditionBase.Terms, t)
+	}
+
+	return conditionBase
+}
+
+// APINrqlAlertCondition is the same as APIConditionBase except that additional fields on the struct are also reviewed.
+func (in NrqlAlertConditionSpec) APINrqlAlertCondition() alerts.NrqlAlertCondition {
+	condition := alerts.NrqlAlertCondition{}
+
+	condition.Description = in.Description
+	condition.Enabled = in.Enabled
+	condition.Name = in.Name
+	condition.Nrql = in.Nrql
+	condition.RunbookURL = in.RunbookURL
+	condition.ViolationTimeLimit = in.ViolationTimeLimit
+
+	for _, term := range in.Terms {
+		t := alerts.NrqlConditionTerms{}
+
+		t.Operator = term.Operator
+		t.Priority = term.Priority
+		// TODO handle the float here
+
+		// When parsing a string
+		f, err := strconv.ParseFloat(term.Threshold, 64)
+		if err != nil {
+			log.Error(err, "strconv.ParseFloat()", "threshold", term.Threshold)
+		}
+
+		t.Threshold = f
+		t.ThresholdDuration = term.ThresholdDuration
+		t.ThresholdOccurrences = term.ThresholdOccurrences
+
+		fmt.Printf("t: %+v\n\n\n", t)
+
+		condition.Terms = append(condition.Terms, t)
+	}
+
+	condition.ID = strconv.Itoa(in.ID)
+	condition.ValueFunction = in.ValueFunction
+
+	return condition
 }
