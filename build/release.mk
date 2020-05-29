c570d8248e653b18536d1694d606e3d425747db1
Sander Blue <sblue@newrelic.com>
2020-05-29 15:43:02 -0700

    chore(release): attempting to keep git clean

--- build/release.mk
+++ build/release.mk
@@ -12,11 +12,13 @@ release: build
 
 release-clean:
 	@echo "=== $(PROJECT_NAME) === [ release-clean    ]: distribution files..."
-	@rm -rfv $(DIST_DIR) $(SRCDIR)/tmp
+	@rm -rfv $(SRCDIR)/tmp
 
 release-publish: clean tools docker-login release-notes
 	@echo "=== $(PROJECT_NAME) === [ release-publish  ]: Publishing release via $(REL_CMD)"
 	$(REL_CMD) --release-notes=$(SRCDIR)/tmp/$(RELEASE_NOTES_FILE)
+
+	@rm -rfv $(SRCDIR)/tmp
 
 # Local Snapshot
 snapshot: release-clean
